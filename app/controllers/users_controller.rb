812fda9c48d22529bc5ee35bb7e70f938088f5f4
learn <kmanoorib@gmail.com>
2019-08-24 01:29:12 -0700

    user#create should make a new user once has_secure_password is in

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,12 +12,30 @@ class UsersController < ApplicationController
     end
 
     def create
+        binding.pry
+        # => <ActionController::Parameters {"name"=>"Adam", "email"=>"adam@gmail.com", "password"=>"password"} 
+        # permitted: false>
+        # @user = User.find_or_create_by(email: user_params[:email])
+        @user = User.find_by(email: user_params[:email])
+        if @user
+            message = "Email already in use. Try another email."
+            render 'new'
+        else
+            @user = User.create(user_params)
+            redirect_to @user # redirect_to user_path(@user)
+        end
     end
 
     def edit
     end
 
     def update
+    end
+    
+    private
+
+    def user_params
+        params.require(:user).permit(:name, :email, :password)
     end
 
 end
