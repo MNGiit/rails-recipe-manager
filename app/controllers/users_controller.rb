e10b4093d495b99207fac29ee2c53e2320346a4c
learn <kmanoorib@gmail.com>
2019-08-23 23:48:26 -0700

    generate Users controller

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,20 @@
+class UsersController < ApplicationController
+    def index
+    end
+
+    def show
+    end
+
+    def new
+    end
+
+    def crete
+    end
+
+    def edit
+    end
+
+    def update
+    end
+    
+end
