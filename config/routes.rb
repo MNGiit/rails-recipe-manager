97890b2e9ea6c9981244e6d61bacd78071329cc6
learn <kmanoorib@gmail.com>
2019-08-24 01:58:17 -0700

    add homepage

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # root 'home'
+  root 'application#home'
+  
   resources :users, only: [:index, :new, :create, :edit, :show]
   # resources :users
   # get '/users', to: 'users#index'
