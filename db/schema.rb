019a7f18ad970d47d4202709710da812855bc705
learn <kmanoorib@gmail.com>
2019-08-25 14:38:46 -0700

    add Recipe, create recipes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_24_062929) do
+ActiveRecord::Schema.define(version: 2019_08_25_213736) do
+
+  create_table "recipes", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
