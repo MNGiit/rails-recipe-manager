2789ba3088460bd98bc88cb607bb0a355bdda24a
learn <kmanoorib@gmail.com>
2019-08-24 18:47:10 -0700

    session assignment from login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,11 @@ class ApplicationController < ActionController::Base
         @user = User.new
     end
 
+    def create
+        binding.pry
+        if @user = User.find_by(email: params[:emaill])
+            session[:user_id] = @user.id
+            redirect_to @user
+        end
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   # root 'home'
   root 'application#home'
-  
+  get '/login', to: 'application#login'
+  post '/login', to: 'application#create'
+
   resources :users, only: [:index, :new, :create, :edit, :show]
   # resources :users
   # get '/users', to: 'users#index'
