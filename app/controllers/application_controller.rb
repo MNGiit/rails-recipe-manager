39eaa7f9c4024317a957c8ed3982d27df6d303f5
learn <kmanoorib@gmail.com>
2019-08-25 14:34:21 -0700

    remove code that's now in session controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,18 +1,2 @@
 class ApplicationController < ActionController::Base
-
-    def login
-        @user = User.new
-    end
-
-    def create
-        @user = User.find_by(email: params[:email])
-        binding.pry
-        if @user && @user.authenticate(params[:password])
-          session[:user_id] = @user.id
-          redirect_to @user # redirect_to user_path(@user)
-        else
-          redirect_to signin_path
-        end
-    end
-
 end
