82ed74d4ebda798bc3a8334731bc2530c874965e
learn <kmanoorib@gmail.com>
2019-08-25 13:30:16 -0700

    user cannot see login page when logged in

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,11 +1,14 @@
 class SessionsController < ApplicationController
     def new
-        @user = User.new
+        if !session[:user_id]
+            @user = User.new
+        else
+            redirect_to root_path
+        end
     end
  
     def create
         @user = User.find_by(email: params[:user][:email])
-        binding.pry
         if @user && @user.authenticate(params[:user][:password])
           session[:user_id] = @user.id
           redirect_to @user # redirect_to user_path(@user)
