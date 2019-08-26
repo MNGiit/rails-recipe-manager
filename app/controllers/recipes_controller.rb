667a3a5244559483dcafd8200be509ff226e91ce
learn <kmanoorib@gmail.com>
2019-08-25 19:29:41 -0700

    #new will redirect user to their show page if user id doesn't match session user id

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -4,8 +4,13 @@ class RecipesController < ApplicationController
     end
 
     def new
+        binding.pry
         if session[:user_id]
-            @recipe = Recipe.new
+            if session[:user_id] == params[:user_id]
+                @recipe = Recipe.new
+            else
+                redirect_to "/users/#{session[:user_id]}"
+            end
         else
             redirect_to login_path
         end
