7f5717efbd0b1790f4f4b7e4167d7fdc60114876
learn <kmanoorib@gmail.com>
2019-08-26 11:00:03 -0700

    example of create table with array

--- db/migrate/20190826175048_create_ingredient_lists.rb
+++ db/migrate/20190826175048_create_ingredient_lists.rb
@@ -2,7 +2,7 @@ class CreateIngredientLists < ActiveRecord::Migration[5.2]
   def change
     create_table :ingredient_lists do |t|
       t.string :name
-
+        t.integer :recipe_id
       t.timestamps
     end
   end
