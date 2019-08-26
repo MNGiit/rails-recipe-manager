777f3e9d795f8df44bc4ff1c88eb622c8c0362a9
learn <kmanoorib@gmail.com>
2019-08-26 11:00:45 -0700

    example of create table with array

--- db/migrate/20190826175048_create_ingredient_lists.rb
+++ db/migrate/20190826175048_create_ingredient_lists.rb
@@ -2,7 +2,16 @@ class CreateIngredientLists < ActiveRecord::Migration[5.2]
   def change
     create_table :ingredient_lists do |t|
       t.string :name
-        t.integer :recipe_id
+    
+    # how to create table with one of the attributes being an array
+    # this is for ingredients, because a recipe has more than 1 ingredient
+    # https://stackoverflow.com/questions/32409820/add-an-array-column-in-rails
+    #  create_table :products do |t|
+    #    t.string :name, null: false
+    #    t.references :category, null: false
+    #    t.text :tags, array: true, default: []
+    #  end
+
       t.timestamps
     end
   end
