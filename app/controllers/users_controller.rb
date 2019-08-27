4bccd9fe61583517964b06bde3f536590ee4f45f
learn <kmanoorib@gmail.com>
2019-08-27 02:52:32 -0700

    add email validation and error printing new page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -20,13 +20,20 @@ class UsersController < ApplicationController
         # => <ActionController::Parameters {"name"=>"Adam", "email"=>"adam@gmail.com", "password"=>"password"} 
         # permitted: false>
         # @user = User.find_or_create_by(email: user_params[:email])
-        @user = User.find_by(email: user_params[:email])
-        if @user
-            message = "Email already in use. Try another email."
-            redirect_to new_user_path # render 'new'
+        # binding.pry # remember there isn't a @user yet
+        @user = User.create(user_params) # testing validations
+        binding.pry
+        # if User.find_by(email: user_params[:email])
+            # message = "Email already in use. Try another email."
+        #     render :new # render 'new'
+        # else
+        #     @user = User.create(user_params)
+        #     redirect_to user_path(@user) # redirect_to @user
+        # end
+        if @user.valid?
+            redirect_to @user # redirect_to user_path(@user)
         else
-            @user = User.create(user_params)
-            redirect_to user_path(@user) # redirect_to @user
+            render '/users/new'# new_user_path # :new
         end
     end
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
     has_secure_password
     has_many :recipes
+    validates :email, uniqueness: { message: "Email in use!" }
+    # there seems to be a validate password already built in, so maybe I don't have to check for presence
 end
