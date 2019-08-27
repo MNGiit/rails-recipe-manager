eee5582413187453c15c5635123b248c774900d8
learn <kmanoorib@gmail.com>
2019-08-26 20:58:05 -0700

    recipe_params tinkering

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -18,7 +18,9 @@ class RecipesController < ApplicationController
     def create
         params[:recipe].merge!(user_id: params[:user_id]) # .merge helps add :user_id to params[:recipe]
         params[:recipe].merge!(ingredients: params[:ingredients])
+        # params[:recipe].permit! can be avoided, look at recipe_params. but this leads to Ingredient.new problem
         params[:recipe].permit! # can't get ingredient_params to work, but this works
+
         # ings = params[:recipe][:ingredients]
 
         # old way where it doesn't get rid of nil values..it works but i want the nil values gone
@@ -39,8 +41,6 @@ class RecipesController < ApplicationController
 
         binding.pry
 
-
-        
         @recipe = Recipe.new(recipe_params)
         @recipe.ingredients = ings
         @recipe.save
@@ -55,6 +55,7 @@ class RecipesController < ApplicationController
     private
 
     def recipe_params
+        # params.require(:recipe).permit(:name, :user_id, { ingredients: :name}) # this works but ingredients causes problems when doing Ingredient.new(recipe_params)
         params.require(:recipe).permit(:name, :user_id)
     end
 
