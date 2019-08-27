db24ead6f2dc273ea7f3494bf2ccbb05809c5ed9
learn <kmanoorib@gmail.com>
2019-08-27 03:15:52 -0700

    extra validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,6 @@ class User < ApplicationRecord
     has_secure_password
     has_many :recipes
     validates :email, uniqueness: { message: "Email in use!" }
+    validates :name, presence: { message: "You forgot to give your name."}
     # there seems to be a validate password already built in, so maybe I don't have to check for presence
 end
