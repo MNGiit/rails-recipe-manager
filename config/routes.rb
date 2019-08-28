2d7018a320efd684174bffb5fa6b6348c85dd916
learn <kmanoorib@gmail.com>
2019-08-27 17:05:06 -0700

    routes has auth callback to sessions#create

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
   get '/login' => 'sessions#new'
   post '/login' => 'sessions#create'
   delete '/logout' => 'sessions#destroy'
+  get '/auth/:provider/callback', to: 'sessions#create'
 
   resources :users, only: [:index, :new, :create, :edit, :show] do
     resources :recipes, only: [:new, :create, :edit, :update]
