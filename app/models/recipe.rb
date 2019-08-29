4509784d53feeb29876385cb4bc3161483ff3e73
learn <kmanoorib@gmail.com>
2019-08-29 00:37:15 -0700

    add form for review, fix error

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -2,7 +2,7 @@ class Recipe < ApplicationRecord
     belongs_to :user
     has_many :ingredients
     has_many :reviews
-    has_many :ratings, through: reviews
+    has_many :ratings, through: :reviews
 
     def self.by_user(user_id)
         where(user: user_id)
