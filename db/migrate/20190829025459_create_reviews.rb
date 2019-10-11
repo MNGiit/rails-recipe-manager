0b6ef0698bf73ef7fda8eebaca1b4a28f8e7e3bb
learn <kmanoorib@gmail.com>
2019-10-11 11:59:50 -0700

    turn ratings score to attribute of review known as score

--- db/migrate/20190829025459_create_reviews.rb
+++ db/migrate/20190829025459_create_reviews.rb
@@ -4,6 +4,7 @@ class CreateReviews < ActiveRecord::Migration[5.2]
       t.belongs_to :user
       t.belongs_to :recipe
 
+      t.integer :score # score should be it's own attribute, not a model
       t.timestamps
     end
   end
