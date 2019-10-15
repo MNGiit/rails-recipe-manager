ee6354a7af0e3c8716367660b867a084a0dccc27
learn <kmanoorib@gmail.com>
2019-10-14 21:38:59 -0700

    review selection now shows previous selected value

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -14,18 +14,34 @@ class ReviewsController < ApplicationController
 
     def create
         whereami = "in #create"
+        # get recipe first
+        @recipe = Recipe.find_by(id: review_params[:recipe_id])
+        # check if recipe already has a review by this user
+        @review = Review.find_or_create_by(user_id: session[:user_id], recipe_id: @recipe.id)
+        @review.score = review_params[:score]
+        @review.save
         binding.pry
+        redirect_to recipe_path(@recipe)
     end
 
     def edit
     end
 
     def update
+        whereami = "in #update"
+        # get recipe first
+        @recipe = Recipe.find_by(id: review_params[:recipe_id])
+        # check if recipe already has a review by this user
+        @review = Review.find_or_create_by(user_id: session[:user_id], recipe_id: @recipe.id)
+        @review.score = review_params[:score]
+        @review.save
+        binding.pry
+        redirect_to recipe_path(@recipe) 
     end
     
     private
 
     def review_params
-        params.require(:review).permit(:score)
+        params.require(:review).permit(:score, :recipe_id)
     end
 end
