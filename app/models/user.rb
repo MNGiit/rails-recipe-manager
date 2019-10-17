f774ee5e688da460d20594ccf6bf23a97eb3b006
learn <kmanoorib@gmail.com>
2019-10-16 21:14:50 -0700

    remove old code

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,6 @@ class User < ApplicationRecord
     has_secure_password
     has_many :recipes
     has_many :reviews
-    has_many :ratings, through: :reviews
     validates :email, uniqueness: { message: "Email in use!" }
     validates :name, presence: { message: "You forgot to give your name."}
     # there seems to be a validate password already built in, so maybe I don't have to check for presence
