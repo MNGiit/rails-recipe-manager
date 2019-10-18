8c53d570c3483f64ec60d659d8816ed804c4a5b9
learn <kmanoorib@gmail.com>
2019-10-18 00:34:37 -0700

    recipe has many recipe_ingredients, and has many ingredients through that

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,9 +1,12 @@
 class Recipe < ApplicationRecord
     belongs_to :user
-    has_many :ingredients
+    # has_many :ingredients # testing out recipe_ingredients
+    has_many recipe_ingredients
+    has_many :ingredients, through :recipe_ingredients
     has_many :reviews
     has_many :ratings, through: :reviews
-    accepts_nested_attributes_for :ingredients
+    # accepts_nested_attributes_for :ingredients # testing out recipe_ingredients
+    accepts_nested_attributes_for :recipe_ingredients
     validates :name, presence: { message: "Give this recipe a name!"}
     scope :order_by_abc, -> { order('name ASC') }
     # scope :red, -> { where(color: 'red') }
@@ -20,6 +23,12 @@ class Recipe < ApplicationRecord
                 ingredient = Ingredient.find_or_create_by(ingredient_attribute)
                 self.ingredients << ingredient
             end
+        end
+    end
+
+    def recipe_ingredients_attributes=(recipe_ingredients_attributes)
+        recipe_ingredients_attributes.values.each do |recipe_ingredient_attributes|
+            # self.recipe_ingredients << recipe_ingredient_attributes
         end
     end
 end
