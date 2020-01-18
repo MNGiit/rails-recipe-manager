25e473cb5440f89e2185267a31aad1d6425fe7d1
learn <kmanoorib@gmail.com>
2020-01-17 18:06:49 -0800

    change associations to better reflect many-to-many

--- app/models/recipe_ingredient.rb
+++ app/models/recipe_ingredient.rb
@@ -1,4 +1,6 @@
 class RecipeIngredient < ApplicationRecord
-    belongs_to :ingredients
-    belongs_to :recipes
+    # belongs_to :ingredients
+    # belongs_to :recipes
+    has_many :recipe_ingredients
+    has_many :recipes, through: :recipe_ingredients
 end
