d3af87e811e1863d269d7572e66773d484d018f6
learn <kmanoorib@gmail.com>
2020-01-20 00:19:55 -0800

    can now create and view recipes with recipe_ingredients

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -1,5 +1,7 @@
 class Ingredient < ApplicationRecord
    # belongs_to :recipes, optional: true
-   belongs_to :recipes
-   belongs_to :recipe_ingredients
+   # belongs_to :recipes
+   # belongs_to :recipe_ingredients
+   has_many :recipe_ingredients
+   has_many :recipes, through: :recipe_ingredients
 end
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -33,13 +33,31 @@ class Recipe < ApplicationRecord
                 ingredient = Ingredient.find_or_create_by(name: recipe_ingredient_attributes[:ingredient_id])
                 recipe_ingredient = RecipeIngredient.new
                 recipe_ingredient.quantity = recipe_ingredient_attributes[:quantity] if recipe_ingredient_attributes[:quantity] != ""
-                recipe_ingredient.ingredient_id = ingredient.id
-                binding.pry
+                ingredient.recipe_ingredients << recipe_ingredient
+                # binding.pry
+                # binding.pry
+                # ingredient = Ingredient.find_or_create_by(name: recipe_ingredient_attributes[:ingredient_id])
+                # recipe_ingredient = RecipeIngredient.new
+                # recipe_ingredient.quantity = recipe_ingredient_attributes[:quantity] if recipe_ingredient_attributes[:quantity] != ""
+                # some reason .save or .find_or_create doesn't actually save ingredient, so I keep getting nil value
+                # ingredient.errors shows me these errors
+                # @base=#<Ingredient:0xb39e27cc id: nil, name: "Cheese", recipe_id: nil, created_at: nil, updated_at: nil>,
+                # @details={:recipes=>[{:error=>:blank}], :recipe_ingredients=>[{:error=>:blank}]},
+                # @messages={:recipes=>["must exist"], :recipe_ingredients=>["must exist"]}>
+                # first i might have to save recipe earlier even though it was working before in the old way
+                # second i'll have recipe_ingredient get saved first and then ingredient
+                # self.save # might save it at the controller so it doesn't get repeated a lot, or before .each
+                # problem seems to be Ingredient.rb and using belongs_to when it shoudl be has_many through
+                
+
+                # recipe_ingredient.ingredient_id = ingredient.id # it seems if ingredient isn't saved, i don't have to use . or dotingredient_id and can just use ingredient
+                # recipe_ingredient.ingredient = ingredient
                 # apply recipe_id if needed. might have to first "save" recipe and then use self.id
                 # apply quantity too
                 # recipe_ingredient.ingredient_id = ingredient works in pry but not in real time?!?!?!
                 # recipe_ingredient.ingredient.id = ingredient # recipe_ingredient.ingredient = ingredient doesn't work, produes error
                 self.recipe_ingredients << recipe_ingredient
+                # binding.pry
             end
         end
     end
