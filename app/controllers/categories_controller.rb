48d06b618e8a4b13d4fe065c51fcc1de6ec8abb4
learn <kmanoorib@gmail.com>
2020-01-24 14:42:16 -0800

    can create new categories

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -8,6 +8,18 @@ class CategoriesController < ApplicationController
     end
 
     def create
+        @category = Category.new(category_params)
+        if @category.valid?
+            @category.save
+            redirect_to @category # redirect_to user_path(@user)
+        else
+            render :new
+        end
+    end
 
+    private
+
+    def category_params
+        params.require(:category).permit(:name)
     end
 end
